b9971e054ef22a0963ec3c97bcf36098ea85a54b
Daniel Westfall <76882475+danielwestfall@users.noreply.github.com>
2021-08-06 11:55:57 -0500

    Create Listmaker.jsx
    Element created to put together lists

---
+++ src/components/Listmaker.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+const Listmaker = ({ info, index, remove }) => {
+
+    const [list, setList] = ListArray([]);
+
+    return (
+        <>
+            {info.thing1}
+        </>
+    )
+}
+
+export default Listmaker
